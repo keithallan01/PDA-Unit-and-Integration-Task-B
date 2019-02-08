fffc4964c55877fb7dfc72d504d7905bebba783d
Keith Allan <keithallan01@gmail.com>
2019-02-08 12:33:11 +0000

    operations can be chained together test passed

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -33,4 +33,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('5')
   })
 
+  it('should do arithmetical operations chained together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('10')
+  })
+
 });
