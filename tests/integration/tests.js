2a766dc662ff5782cce03bbfd9c0d388c2c35e19
Keith Allan <keithallan01@gmail.com>
2019-02-08 12:29:53 +0000

    performs arithmetical operations and displays the results

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -24,4 +24,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('123')
   })
 
+  it('should do arithmetical operations update the display with the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
+  })
+
 });
