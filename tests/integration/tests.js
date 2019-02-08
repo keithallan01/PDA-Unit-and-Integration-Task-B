ac9ac0ac8e48a1d35b9da3708538a21da93d2077
Keith Allan <keithallan01@gmail.com>
2019-02-08 12:55:23 +0000

    if divided by zero, calculator displays NaN

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -69,4 +69,14 @@ describe('calculator functionality', function() {
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('9999800001')
   })
+
+  it('should be should display NaN if divided by zero', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('NaN')
+  })
+
 });
